5ded117fc6797d9aa7409c483049e3b1f08babf7
rameshdhage <dhageramesh143@gmail.com>
2024-02-19 13:30:37 +0530

    add the logic for find even and odd number

--- src/main/java/com/java8feature/HighestNumber.java
+++ src/main/java/com/java8feature/HighestNumber.java
@@ -2,12 +2,20 @@ package com.java8feature;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HighestNumber {
 
 	public static void main(String[] args) {
 	  List<Integer>  number=Arrays.asList(3,3,2,4,7,8,9,10);
 	  
+List<Integer> evennumber=number.stream().filter(num->num%2==0).collect(Collectors.toList());
+	  
+List<Integer> oddNumber=number.stream().filter(num->num%2==1).collect(Collectors.toList());
+	
+	System.out.println(oddNumber);
+	
+	System.out.println(evennumber);
 	Integer max= number.stream().max((i,j)->i.compareTo(j)).get();
 	  
 	  System.out.println(max);
